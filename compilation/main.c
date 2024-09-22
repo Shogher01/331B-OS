a524a475e6335a6f2e396a0c709419b8fdf7ea4a
Shogher01 <shogher.galstyan@mail.ru>
2024-09-22 13:37:10 +0000

    Compilation hw source files

---
+++ compilation/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "math_utils.h"
+
+int main(){
+	int a = 3;
+	int a_sqr = sqr(a);
+	printf("%d\n", a_sqr);
+
+	return 0;
+
+}
---
+++ compilation/math_utils.c
@@ -0,0 +1,5 @@
+#include "math_utils.h"
+
+int sqr(int a) {
+	return a*a;
+}
